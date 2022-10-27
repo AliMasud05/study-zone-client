b3e6c4df928e1a4aa818a8c803b12460d1e5d6ac
AliMasud05 <morshedalimasud3@gmail.com>
2022-10-27 23:38:32 +0600

    login

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,6 +1,52 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 const Login = () => {
+
+    const [error, setError] = useState('');
+    const { signIn, setLoading } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const from = location.state?.from?.pathname || '/';
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+                if (user.emailVerified) {
+                    navigate(from, { replace: true });
+                }
+                else {
+                    alert('Your email is not verified. Please verify your email address.')
+                }
+            })
+            .catch(error => {
+                console.error(error)
+                setError(error.message);
+            })
+            .finally(() => {
+                setLoading(false);
+            })
+    }
+
+
+
+
+
+
+
+
+
     return (
         <div className=' flex justify-center'>
             <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 dark:bg-gray-900 dark:text-gray-100">
@@ -8,17 +54,18 @@ const Login = () => {
                     <h1 className="my-3 text-4xl font-bold">Sign in</h1>
                     <p className="text-sm dark:text-gray-400">Sign in to access your account</p>
                 </div>
-                <form novalidate="" action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
+                <form onSubmit={handleSubmit}  action="" className="space-y-12 ng-untouched ng-pristine ng-valid">
                     <div className="space-y-4">
                         <div>
-                            <label for="email" className="block mb-2 text-sm">Email address</label>
+                            <label htmlFor="email" className="block mb-2 text-sm">Email address</label>
                             <input type="email" name="email" id="email" placeholder="leroy@jenkins.com" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
                         </div>
                         <div>
                             <div className="flex justify-between mb-2">
-                                <label for="password" className="text-sm">Password</label>
-                                <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">Forgot password?</a>
+                                <label htmlFor="password" className="text-sm">Password</label>
+                                <a  href="#" className="text-xs hover:underline dark:text-gray-400">Forgot password?</a>
                             </div>
+                            <p>{error}</p>
                             <input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 dark:bg-gray-900 dark:text-gray-100" />
                         </div>
                     </div>
@@ -27,10 +74,11 @@ const Login = () => {
                             <button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">Sign in</button>
                         </div>
                         <p className="px-6 text-sm text-center dark:text-gray-400">Don't have an account yet?
-                            <a rel="noopener noreferrer" href="#" className="hover:underline dark:text-violet-400">Sign up</a>.
+                            <a className="hover:underline dark:text-violet-400">Sign up</a>.
                         </p>
                     </div>
                 </form>
+
             </div>
         </div>
     );
