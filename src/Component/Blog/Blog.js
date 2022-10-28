49562927830862bcf430c7aa0a744cb1c7d6477a
AliMasud05 <morshedalimasud3@gmail.com>
2022-10-28 23:49:14 +0600

    submit

--- src/Component/Blog/Blog.js
+++ src/Component/Blog/Blog.js
@@ -4,7 +4,7 @@ const Blog = () => {
     return (
         <div>
           <div>
-            <h2>What is 'cors'? </h2>
+            <h2 className='text-2xl'>What is 'cors'? </h2>
           <p>CORS stands for Cross-Origin Resource Sharing.
 
             Normally, requesting resources across domains is a security risk. If a random site could pull data into JavaScript from another, the random site could "steal" information that way. If the target site were, say, your bank's website, perhaps just visiting such a site could get sensitive information leaked.
@@ -14,21 +14,21 @@ const Blog = () => {
             If sites don't have any sensitive data, they can return a wildcard origin "*" to indicate that any origin may request data. For example, a public API for a service with no login system might be perfectly OK for anyone to pull from, anywhere, and so wildcard CORS headers would be appropriate for it.</p>
           </div>
           <div>
-            <h2>Why are you using firebase ? What other options do you have to implement
+          <h2 className='text-2xl'>Why are you using firebase ? What other options do you have to implement
               authentication?</h2>
           <p>Starting with the most obvious reason - Firebase has a really good free tier. The so-called "Spark plan" provides you with quite generous usage limits (one of the top offerings in the business as far as I know) for all of Firebase products. You can find more details on the pricing page.</p>
           <p>From hosting and database, through static storage, messaging, analytics, and app distribution to cloud functions and machine learning, Firebase is truly an all-in-one solution. Sure, you can always go and find some better alternatives for each of these products individually, but I guarantee you won't achieve the same level of integration and polish between them, as you would with Firebase.</p>
           <p>Easy API</p>
           <p>Great docs & community</p>
           <p>"Serverless standard"</p>
-          <h3>other option of Authentication:</h3>
+          <h3 className='text-2xl'>other option of Authentication:</h3>
           <p>Okta</p>
           <p>Supabase</p>
           <p>Ory</p>
           <p>STYTCH</p>
           </div>
           <div>
-             <h2>How does the private route work?
+          <h2 className='text-2xl'>How does the private route work?
             </h2>
           <p>A classic example would be a site that has a landing page, various marketing pages, a login page, and then an app section for logged-in users. The logged-in section doesn’t need to be server rendered as all data will be loaded live from your API after the user logs in. So it makes sense to make this portion of your site client-only.
 
